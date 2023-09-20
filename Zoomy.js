6fdb8b0014910b0cc52bb459759d2b60e4251a14
pmad01 <patrickmadhi@gmail.com>
2023-09-20 15:21:18 +0200

    practice

---
+++ Zoomy.js
@@ -0,0 +1,408 @@
+export default class Zoomy {
+	/**
+	 *The HTML element we want to transform
+	 * @type {HTMLElement}
+	 */
+	el;
+
+	/**
+	 *The box element that contains the image
+	 * @type {HTMLElement}
+	 */
+	boxEl;
+
+	/**
+	 * Checks if the element is enabled or not
+	 * @type {boolean}
+	 */
+	enabled = true;
+
+	/**
+	 * Checks if mouse is clicked
+	 * @type {boolean}
+	 */
+	mouseIsDown = false;
+
+	/**
+	 * Checks if we are moving the image
+	 * @type {boolean}
+	 */
+	isDragging = false;
+
+	/**
+	 * The X coordinate of the cursor after the previous transformation
+	 * @type {number}
+	 */
+	lastMouseX = 0;
+
+	/**
+	 * The Y coordinate of the cursor after the previous transformation
+	 * @type {number}
+	 */
+	lastMouseY = 0;
+
+	/**
+	 * Options applicable to an instance of the class
+	 * @type {Object}
+	 */
+	options = {
+		zoomUpperConstraint: null
+	};
+
+	/**
+	 * The original z-index of the image before any change
+	 * @type {string}
+	 */
+	originalZIndexValue;
+
+	/**
+	 * The original transform property value of the image
+	 * @type {string}
+	 */
+	originalTransformValue;
+
+	/**
+	 * @param elementId
+	 * @param {object} options
+	 */
+	constructor(elementId, options) {
+		this.el = document.getElementById(elementId);
+		this.options = {...options };
+		this.boxEl = this.el.parentElement;
+		this.boxEl.style.overflow = 'hidden';
+		this.options.zoomUpperConstraint ||= 4;
+		var fn = this.handleMouseEvents.bind(this);
+		this.originalZIndexValue = window.getComputedStyle(this.el).zIndex;
+		this.originalTransformValue = window.getComputedStyle(this.el).transform;
+
+		['mousemove', 'mouseup'].forEach(
+			event => {
+				document.addEventListener(event, fn, {passive: false});
+			}
+		);
+
+		this.boxEl.addEventListener('wheel', (e) => {
+			this.el.style.zIndex = '999999';
+			fn(e);
+		}, {passive: false});
+
+		this.boxEl.addEventListener('mouseleave',  () => {
+			if (!this.isDragging) {
+				this.el.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
+				this.el.style.transform = this.originalTransformValue;
+				this.el.style.zIndex = this.originalZIndexValue;
+			}
+		});
+
+		this.el.addEventListener('mousedown', fn, {passive: false});
+
+	}
+
+	/**
+	 * 'Resize and move image' (output) on screen based on 'mouse action' (input)
+	 * @param {Event} e
+	 */
+	transformByMouseEvent(e) {
+		var currentMouseX = e.x,
+			currentMouseY = e.y,
+			moveXBy = 0,
+			moveYBy = 0,
+			img = this.el.getBoundingClientRect(),
+			box = this.boxEl.getBoundingClientRect(),
+			widthFits = img.width < box.width,
+			heightFits = img.height < box.height,
+			currentLeft = img.left,
+			currentRight = img.right,
+			currentTop = img.top,
+			currentBottom = img.bottom;
+
+		this.isDragging = false;
+		if (e.type === 'mousedown') {
+			this.mouseIsDown = true;
+		}
+		if (e.type === 'mouseup') {
+			this.mouseIsDown = false;
+		}
+		if (this.mouseIsDown && (e.type === 'mousemove' || e.type === 'mouseleave')) {
+			this.isDragging = true;
+		}
+
+		if (this.isDragging) {
+			var newLeft = currentLeft + (currentMouseX - this.lastMouseX),
+			newRight = currentRight + (currentMouseX - this.lastMouseX),
+			newTop = currentTop + (currentMouseY - this.lastMouseY),
+			newBottom = currentBottom + (currentMouseY - this.lastMouseY),
+			heightInsideCanvas = (img.top >= box.top && img.bottom <= box.bottom),
+		    widthInsideCanvas = (img.left >= box.left && img.right <= box.right);
+
+			//flag to check if image can freely be moves on the x-axis(horizontally)
+			var freeX = (
+					img.width > box.width || // image overflows canvas
+					!widthInsideCanvas // or simply positioned off canvas
+				) &&
+				!(
+					(newLeft < box.left && //hard left wall
+						(
+							newLeft <= currentLeft && //if going left
+							newRight <= box.right //or right edge is visible
+						)
+					) ||
+					(newRight > box.right && //hard right wall
+						(
+							newRight >= currentRight && //if going right
+							newLeft >= box.left //or left edge is visible
+						)
+					)
+				);
+
+			//flag to check if image can freely be moves on the y-axis(vertically)
+			var freeY = (
+				img.height > box.height || // image bigger than canvas
+				!heightInsideCanvas // or image outside of canvas
+			) &&
+			!(
+				(newTop < box.top && //hard ceiling
+					(
+						newTop <= currentTop && //if going up
+						newBottom <= box.bottom //or bottom is visible
+					)
+				) ||
+				(newBottom > box.bottom && //hard floor
+					(
+						newBottom >= currentBottom && //if going down
+						newTop >= box.top //or top is visible
+					)
+				)
+			);
+
+			if (freeX) {
+				moveXBy = currentMouseX - this.lastMouseX;
+			}
+			if (freeY) {
+				moveYBy = currentMouseY - this.lastMouseY;
+			}
+		}
+
+		if (e.deltaY) {
+			var imgCenterX = img.left + (img.width / 2),
+				imgCenterY = img.top + (img.height / 2),
+				boxCenterX = box.left + (box.width / 2),
+				boxCenterY = box.top + (box.height / 2),
+				newImageCenterXDiff = currentMouseX - imgCenterX,
+				newImageCenterYDiff = currentMouseY - imgCenterY,
+				zoomInImage = this.boxEl.contains(e.target),
+				isShrinking = e.deltaY > 0,
+				enlargeOrShrinkBy = 0,
+				zoomFactor = .1,
+				currentScale = Math.round((img.width / this.el.offsetWidth) * 10) / 10,
+				fitsEntirely = widthFits && heightFits,
+				sideFits = widthFits || heightFits;
+			console.log({
+				imgWidth: img.width,
+				offsetWidth: this.el.offsetWidth,
+				currentScale
+			})
+
+			enlargeOrShrinkBy = Math.round(((e.deltaY > 0 ? -zoomFactor : zoomFactor) * currentScale) * 10) / 10;
+			// console.log(enlargeOrShrinkBy)
+
+			var newScale = currentScale + enlargeOrShrinkBy,
+				newWidth = this.el.offsetWidth * newScale,
+				newHeight = this.el.offsetHeight * newScale;
+
+			if (newScale > this.options.zoomUpperConstraint) {
+				enlargeOrShrinkBy = 0;
+			}
+			else if (currentScale <= 1 && isShrinking) {
+				enlargeOrShrinkBy = 0;
+				this.el.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
+				this.el.style.transform = this.originalTransformValue;
+			}
+			else {
+				if (zoomInImage) {
+					if (!fitsEntirely) {
+						//ratio of distance to scaling
+						var adjustedDiffX = newImageCenterXDiff / currentScale,
+							adjustedDiffY = newImageCenterYDiff / currentScale;
+
+						moveXBy = -adjustedDiffX * enlargeOrShrinkBy,
+						moveYBy = -adjustedDiffY * enlargeOrShrinkBy;
+					}
+					if (isShrinking) {
+						if (sideFits) {
+							diffX = imgCenterX - boxCenterX,
+							diffY = imgCenterY - boxCenterY,
+							scaleDiff = 1 - currentScale;
+							// console.log(imgCenterX, imgCenterY)
+
+							//ratio of distance to scaling
+							adjustedDiffX = diffX / scaleDiff,
+							adjustedDiffY = diffY / scaleDiff,
+							moveXBy = -adjustedDiffX * enlargeOrShrinkBy,
+							moveYBy = -adjustedDiffY * enlargeOrShrinkBy;
+						}
+						var widthShrankBy = newWidth - img.width,
+							oneSideWidthShrankBy = widthShrankBy / 2,
+							heightShrankBy = newHeight - img.height,
+							oneSideHeightShrankBy = heightShrankBy / 2,
+							horizontalMove = moveXBy - oneSideWidthShrankBy,
+							verticalMove = moveYBy - oneSideHeightShrankBy,
+							boxPadding = 0;
+
+						newLeft = currentLeft + horizontalMove;
+						newRight = newLeft + newWidth;
+						newTop = currentTop + verticalMove;
+						newBottom = newTop + newHeight;
+
+						if (!widthFits) {
+							if (newLeft > currentLeft) {
+								var newLeftDistance = box.left - newLeft,
+									movesRightTooMuch = newLeftDistance < -boxPadding,
+									paddedBoxLeft = box.left + boxPadding,
+									leftSideIsVisible = currentLeft > paddedBoxLeft + 1;
+
+								if (movesRightTooMuch && !leftSideIsVisible) {
+									moveXBy += newLeftDistance + boxPadding;
+								}
+								// console.log({
+								// 	newLeftDistance,
+								// 	currentLeft,
+								// 	boxLeft: box.left,
+								// 	visualMargin: boxPadding,
+								// 	movesRightTooMuch,
+								// 	leftSideIsVisible,
+								// 	moveXBy
+								// });
+							}
+
+							if (newRight < currentRight) {
+								var newRightDistance = box.right - newRight,
+									movesLeftTooMuch = newRightDistance > boxPadding,
+									paddedBoxRight = box.right - boxPadding,
+									rightSideIsVisible = currentRight < paddedBoxRight - 1;
+								if (movesLeftTooMuch && !rightSideIsVisible) {
+									moveXBy += newRightDistance - boxPadding;
+								}
+							}
+						}
+
+						if (!heightFits) {
+							if (newTop > currentTop) {
+								var newTopDistance = box.top - newTop,
+									movesDownTooMuch = newTopDistance < -boxPadding,
+									paddedBoxTop = box.top + boxPadding,
+									topIsVisible = currentTop > paddedBoxTop + 1;
+								if (movesDownTooMuch && !topIsVisible) {
+									moveYBy += newTopDistance + boxPadding;
+								}
+							}
+
+							if (newBottom < currentBottom) {
+								var newBottomDistance = box.bottom - newBottom,
+									movesUpTooMuch = newBottomDistance > boxPadding,
+									paddedBoxBottom = box.bottom - boxPadding,
+									bottomIsVisible = currentBottom < paddedBoxBottom - 1;
+								if (movesUpTooMuch && !bottomIsVisible) {
+									moveYBy += newBottomDistance - boxPadding;
+								}
+							}
+						}
+					}
+				} else {
+					var diffX = imgCenterX - boxCenterX,
+						diffY = imgCenterY - boxCenterY,
+						scaleDiff = 1 - currentScale;
+
+					//ratio of distance to scaling
+					adjustedDiffX = diffX / scaleDiff,
+					adjustedDiffY = diffY / scaleDiff,
+					moveXBy = -adjustedDiffX * enlargeOrShrinkBy,
+					moveYBy = -adjustedDiffY * enlargeOrShrinkBy;
+				}
+			}
+		}
+
+		this.transform(this.el, moveXBy, moveYBy, enlargeOrShrinkBy);
+		e.preventDefault();
+
+		this.lastMouseX = currentMouseX;
+		this.lastMouseY = currentMouseY;
+	}
+
+	/**
+	 * This gets the transformation matrix data in real time
+	 * @return {{scaleX: number, scaleY: number, translateY: number, translateX: number, skewX: number, skewY: number}}
+	 */
+	getMatrix() {
+		var matrix = this.el.style.transform?.substring(7);
+		if (matrix) {
+			matrix = matrix.slice(0, matrix.length - 1).split(',').map(parseFloat);
+		} else {
+			matrix = [1, 0, 0, 1, 0, 0];
+		}
+		return {
+			scaleX: matrix[0],
+			skewY: matrix[1],
+			skewX: matrix[2],
+			scaleY: matrix[3],
+			translateX: matrix[4],
+			translateY: matrix[5]
+		};
+	}
+
+	/**
+	 * Side effect: transform an element's geometrical visual appearance
+	 * @param {HTMLElement} el
+	 * @param {int} moveXBy How much to move left-right, from where it already is. 0 leaves it as it is.
+	 * @param {int} moveYBy How much to move up-down, from where it already is. 0 leaves it as it is.
+	 * @param {number} enlargeOrShrinkBy How big-small it should get, compared to however it already is. In percentage compared to baseline. 1 being 100%. 0 leaves it as it is.
+	 * @return void
+	 */
+	transform(el, moveXBy = 0, moveYBy = 0, enlargeOrShrinkBy = 0) {
+		var m = this.getMatrix();
+		m.translateX += moveXBy;
+		m.translateY += moveYBy;
+		m.scaleX += enlargeOrShrinkBy;
+		m.scaleY += enlargeOrShrinkBy;
+		this.el.style.transform = 'matrix('+Object.values(m).join(', ')+')';
+	}
+
+	/**
+	 * This method removes the event listeners from the instance element of the class.
+	 */
+	detach(){
+		['mousemove', 'mouseup'].forEach(
+			event => document.removeEventListener(event, this.handleMouseEvents)
+		);
+		['wheel', 'mouseleave'].forEach(
+			event => this.boxEl.removeEventListener(event, this.handleMouseEvents)
+		);
+		this.el.removeEventListener('mousedown', this.handleMouseEvents);
+	}
+
+	/**
+	 * This method enables the instance of the class
+	 */
+	enable(){
+		this.enabled = true;
+	}
+
+	/**
+	 * This method disables the instance of the class
+	 */
+	disable(){
+		this.enabled  = false;
+	}
+
+	/**
+	 * Handles all mouse events
+	 * @param {MouseEvent} e
+	 * @return {boolean}
+	 */
+	handleMouseEvents(e) {
+		if(!this.enabled){
+			return false;
+		}
+		this.transformByMouseEvent(e);
+	}
+}
--- script.js
+++ script.js
@@ -1,95 +1,195 @@
-const articlesContainer = document.getElementById('articlesContainer');
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const renderNews = function (countryArticles) {
-	if  (!countryArticles.length) {
-		articlesContainer.insertAdjacentHTML('beforeend', 'No articles available for selected country');
-	} else {
-		countryArticles.forEach(article => {
-			const title = article.title;
-			const content = article.content ? article.content : article.description;
-			const imgSrc = article.urlToImage;
+import Zoomy from './Zoomy.js';
+class NewsApp {
+	/**
+	 * The container that holds all articles
+	 * @type {HTMLDivElement}
+	 */
+	articlesContainer;
 
-			const html = `
-				<div class="container">
-    			<h2>${title}</h2>
-    			<div class="box" style="float: left; width:340px; height:340px;">
-        			<img id="zoomy1" width="340" src="${imgSrc}" alt=""/>
-    			</div>
-    				<p class="text">${content}</p>
-				</div>
-		`
-			articlesContainer.insertAdjacentHTML('beforeend', html);
-		});
+	/**
+	 * Button that submits country name value
+	 * @type {HTMLButtonElement}
+	 */
+	btn1;
+
+	/**
+	 * Button that submits topic information
+	 * @type {HTMLButtonElement}
+	 */
+	btn2;
+
+	/**
+	 * The container that holds input fields and buttons to search
+	 * @type {HTMLDivElement}
+	 */
+	searchContainer;
+
+	/**
+	 * The search field for country name
+	 * @type {HTMLInputElement}
+	 */
+	countryNameInput;
+
+	/**
+	 * The search field for topic
+	 * @type {HTMLInputElement}
+	 */
+	topicInput;
+	constructor() {
+		this.articlesContainer = document.getElementById('articlesContainer');
+		this.btn1 = document.getElementById('btn1');
+		this.btn2 = document.getElementById('btn2');
+		this.searchContainer = document.getElementById('countrySearch');
+		this.countryNameInput = document.getElementById('countryName');
+		this.topicInput = document.getElementById('topic');
+
+		this.btn1.addEventListener('click', this.countryNews.bind(this));
+		this.btn2.addEventListener('click', this.topicNews.bind(this));
+	}
+
+	/**
+	 * Render the news on the page
+	 * @param {Array} countryArticles
+	 */
+	renderNews(countryArticles) {
+		this.articlesContainer.innerHTML = ''; // Clear existing content
+		if (!countryArticles.length) {
+			this.articlesContainer.insertAdjacentHTML('beforeend', 'No articles available for selected country');
+		} else {
+			countryArticles.forEach(article => {
+				const title = article.title;
+				const content = article.content ? article.content : article.description;
+				const imgSrc = article.urlToImage;
+
+				const html = `
+          			<div class="container">
+            			<h2>${title}</h2>
+            			<div class="box" style="float: left;">
+              				<img class="zoomable" width="340" src="${imgSrc}" alt=""/>
+            			</div>
+                        <p class="text">${content}</p>
+          			</div>
+        		`;
+				this.articlesContainer.insertAdjacentHTML('beforeend', html);
+			});
+			const zoomableImages = document.querySelectorAll('.zoomable');
+			let i = 0;
+			zoomableImages.forEach(image => {
+				i++;
+				image.setAttribute('id', 'zoomable' + i);
+				new Zoomy(image.id, {
+					zoomUpperConstraint: 4 // You can adjust this constraint as needed
+				});
+			});
+		}
+	}
+
+	/**
+	 * Render the error on the page
+	 * @param {String} errorMsg
+	 */
+	renderError(errorMsg) {
+		this.articlesContainer.innerHTML = errorMsg;
+	}
+
+	/**
+	 * Fetch country information
+	 * @param {String} countryName
+	 * @return {Promise<*>}
+	 */
+	async fetchCountry(countryName) {
+		try {
+			const res = await fetch('https://restcountries.com/v3.1/name/' + countryName);
+			if (!res.ok) {
+				throw new Error('Problem getting country data, check country name.');
+			}
+			const data = await res.json();
+			return data[0];
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	/**
+	 * Transform the country name from a string to ISO 3166-1 alpha 2 format(two letter)
+	 * @return {Promise<string>}
+	 */
+	async getTwoLetterCountry() {
+		const countryName = this.countryNameInput.value;
+		const country = await this.fetchCountry(countryName);
+		const twoLetterCountry = country.cca2.toLowerCase();
+		return twoLetterCountry;
+	}
+
+	/**
+	 * Fetch the news of the country given on the country input field
+	 * @return {Promise<*>}
+	 */
+	async fetchCountryNews() {
+		try {
+			const countryCode = await this.getTwoLetterCountry();
+			const apiKey = config.API_KEY; // Make sure you have the API_KEY defined somewhere
+			const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${apiKey}`);
+			if (!res.ok) {
+				throw new Error('Problem getting country news, check country code');
+			}
+			const data = await res.json();
+			return data.articles;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	/**
+	 * Render the news of a country in the page and check/handle errors
+	 * @return {Promise<void>}
+	 */
+	async countryNews() {
+		try {
+			const countryNews = await this.fetchCountryNews();
+			this.renderNews(countryNews);
+		} catch (err) {
+			console.error(err.message);
+			this.renderError(err.message);
+		} finally {
+			this.searchContainer.style.visibility = 'hidden';
+		}
+	}
+
+	/**
+	 * Fetch the news of the topic given on the topic input field
+	 * @return {Promise<*>}
+	 */
+	async fetchTopicNews() {
+		try {
+			const topic = this.topicInput.value;
+			const apiKey = config.API_KEY; // Make sure you have the API_KEY defined somewhere
+			const res = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2023-09-17&sortBy=publishedAt&language=en&apiKey=${apiKey}`);
+			if (!res.ok) {
+				throw new Error('Problem getting topic news, check topic');
+			}
+			const data = await res.json();
+			return data.articles;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	/**
+	 * Render the news of a topic in the page and check/handle errors
+	 * @return {Promise<void>}
+	 */
+	async topicNews() {
+		try {
+			const topicNews = await this.fetchTopicNews();
+			this.renderNews(topicNews);
+		} catch (err) {
+			console.error(err.message);
+			this.renderError(err.message);
+		} finally {
+			this.searchContainer.style.visibility = 'hidden';
+		}
 	}
 }
 
-const renderError = function (errorMsg) {
-	articlesContainer.insertAdjacentHTML('beforeend', errorMsg);
-}
-const fetchCountry = async function(countryName)  {
-	const res = await fetch('https://restcountries.com/v3.1/name/' + countryName);
-	if (!res.ok) {
-		throw new Error('Problem getting country data, check country name.')
-	}
-	return res.json();
-}
-const getTwoLetterCountry = async function() {
-	const countryName = document.getElementById('countryName').value;
-	const [country] = await fetchCountry(countryName);
-	const twoLetterCountry = country.cca2;
-	return twoLetterCountry;
-}
-
-const fetchCountryNews = async function() {
-	let countryCode = await getTwoLetterCountry();
-	countryCode = countryCode.toLowerCase();
-	const apiKey = config.API_KEY;
-	const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${apiKey}`);
-	if (!res.ok) {
-		throw new Error('Problem getting country news, check country code');
-	}
-	return res.json();
-}
-const countryNews = async function() {
-	try {
-		const countryNews = await fetchCountryNews();
-		const articles = countryNews.articles;
-		console.log(articles)
-		renderNews(articles);
-	} catch (err) {
-		console.error(err.message);
-		renderError(err.message);
-	} finally {
-		const countrySearchDiv = document.getElementById('countrySearch');
-		countrySearchDiv.style.visibility = 'hidden';
-	}
-}
-
-const fetchTopicNews = async function() {
-	const topic = document.getElementById('topic').value;
-	const apiKey = config.API_KEY;
-	const res = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2023-09-17&sortBy=publishedAt&language=en&apiKey=${apiKey}`);
-	if (!res.ok) {
-		throw new Error('Problem getting topic news, check topic');
-	}
-	return res.json();
-}
-
-const topicNews = async function() {
-	try {
-		const topicNews = await fetchTopicNews();
-		const articles = topicNews.articles;
-		console.log(articles)
-		renderNews(articles);
-	} catch (err) {
-		console.error(err.message);
-		renderError(err.message);
-	} finally {
-		const countrySearchDiv = document.getElementById('countrySearch');
-		countrySearchDiv.style.visibility = 'hidden';
-	}
-}
-
-btn1.addEventListener('click', countryNews);
-btn2.addEventListener('click', topicNews);
+const newsApp = new NewsApp();
